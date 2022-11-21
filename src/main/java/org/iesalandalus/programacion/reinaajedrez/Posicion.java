04983e2b389218d6442403682b3deed640c395ee
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-21 21:49:27 +0100

    creados getter y setter de los atributos fila y columna

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -4,5 +4,22 @@ public class Posicion {
 	//declaracion de atributos
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8) 
+			throw new IllegalArgumentException("ERROR: fila no valida.");
+		this.fila = fila;
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h')
+			throw new IllegalArgumentException("ERROR: columna no valida.");
+		this.columna = columna;
+	}
 
 }
