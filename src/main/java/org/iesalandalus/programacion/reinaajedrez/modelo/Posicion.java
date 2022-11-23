e331365a8b2a7b09f7d84c27e1aa0cc368070b7d
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-23 19:18:17 +0100

    h

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -15,7 +15,7 @@ public class Posicion {
 	
 	public Posicion (Posicion p) {
 		if (p==null)
-			throw new NullPointerException("ERROR: No se puede copiar una posicion nula.");
+			throw new NullPointerException("ERROR: No es posible copiar una posición nula.");
 		setFila (p.getFila());
 		setColumna (p.getColumna());
 	}
@@ -26,7 +26,7 @@ public class Posicion {
 	}
 	private void setFila(int fila) {
 		if (fila < 1 || fila > 8) 
-			throw new IllegalArgumentException("ERROR: fila no valida.");
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		this.fila = fila;
 	}
 	public char getColumna() {
@@ -34,7 +34,7 @@ public class Posicion {
 	}
 	private void setColumna(char columna) {
 		if (columna != 'a' && columna != 'b' && columna != 'c' && columna != 'd' && columna != 'e' && columna != 'f' && columna != 'g' && columna != 'h')
-			throw new IllegalArgumentException("ERROR: columna no valida.");
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
 		this.columna = columna;
 	}
 	@Override
