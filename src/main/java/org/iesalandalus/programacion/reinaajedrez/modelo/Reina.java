8388b752891284d3014a2c715573b744979cb19f
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-23 12:15:44 +0100

    creado constructor por defecto

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -3,6 +3,7 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Reina {
 	private Color color;
 	private Posicion posicion;
+	Posicion posicionReinaBlanca=new Posicion(1, 'd');
 	
 	public Color getColor() {
 		return color;
@@ -20,5 +21,11 @@ public class Reina {
 			throw new NullPointerException("ERROR: La posicion no puede ser null.");
 		this.posicion = posicion;
 	}
+	public Reina (){
+		 setColor (Color.BLANCO) ;
+		 setPosicion(posicionReinaBlanca);
+		
+	}
+	
 	
 }
