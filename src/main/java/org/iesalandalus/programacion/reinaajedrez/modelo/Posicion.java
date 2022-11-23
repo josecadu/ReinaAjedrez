12a8ea506a210e99eff79882927f40c2fe9d6143
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-23 11:33:49 +0100

    modificado el cambio en posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -46,7 +46,7 @@ public class Posicion {
 		return columna;
 	}
 	private void setColumna(char columna) {
-		if (columna < 'a' || columna > 'h')
+		if (columna != 'a' && columna != 'b' && columna != 'c' && columna != 'd' && columna != 'e' && columna != 'f' && columna != 'g' && columna != 'h')
 			throw new IllegalArgumentException("ERROR: columna no valida.");
 		this.columna = columna;
 	}
