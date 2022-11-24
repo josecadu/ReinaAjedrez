a9cebff1ffbc2a1cf5dbd09ccb9759622d6f7d23
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-24 15:03:01 +0100

    creado metodo ejecutar opcion crearReinaDefecto y crearReinaColor

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 
 public class MainApp {
@@ -10,5 +11,29 @@ public class MainApp {
 		
 		
 	}
-	ejecutarOpcion
+	private static void ejecutarOpcion()
+	{
+		int opEjecutar;
+		switch (opEjecutar) {
+		case 1:
+			
+			break;
+		case 2:
+		
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+	private static void crearReinaDefecto()
+	{
+		Reina reina= new Reina();
+	}
+	private static void crearReinaColor()
+	{
+		Reina reinaElig= new Reina (Consola.elegirOpcion());
+	}
+	private static void 
 }
