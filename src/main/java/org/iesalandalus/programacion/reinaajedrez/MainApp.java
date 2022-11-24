e0b8d6db48c4ad2a7cb1be2746437528d49a1505
pauli <pauli@DESKTOP-3HSMRRE>
2022-11-24 14:22:50 +0100

    creado atributo de clase reina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,14 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
+	
+	private static Reina reina;
 	
 	public static void main(String[] args) {
 		
+		
 	}
+	ejecutarOpcion
 }
